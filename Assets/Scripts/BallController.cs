cd895a41e0f058d0f03d233566f560acd136ec9b
zakcrust <akbarhida@gmail.com>
2020-06-19 17:11:17 +0700

    Gameplay sudah dibuat

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject lantai;
+    public GameObject tempatMuncul;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < lantai.transform.position.z - 1)
+        {
+            transform.position = tempatMuncul.transform.position;
+        }
+    }
+}
